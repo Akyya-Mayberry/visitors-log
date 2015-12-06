e901f913ea9dac562f98a60d057a26ba0375fd43
Fuzzbuzz <hollywoodno@users.noreply.github.com>
2015-12-05 19:49:13 -0800

    Update is_in_or_out.php

--- html/models/is_in_or_out.php
+++ html/models/is_in_or_out.php
@@ -3,9 +3,7 @@
 //DATABASE
 //working with the database
 function isLoggedIn($visitorFirst, $visitorLast, $visitingFirst, $visitingLast, $clockedIn, $clockedInDb) {	
-   
-      
-    
+  //add database crendentials here
         $conn = new mysqli($servername, $username, $password);
         if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
